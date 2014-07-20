6fdda4d9d1ec927abc2548d37790d7822f765009
reppard <reppardwalker@gmail.com>
2014-07-20 08:08:40 -0400

    added group member name sorting

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ActiveRecord::Base
   has_many :memberships
   has_many :members, :through => :memberships
+
+  def members_sorted
+    self.members.sort_by{ |member| member.name }
+  end
 end
