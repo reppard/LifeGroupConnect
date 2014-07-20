fb53f35c2a253b685054f028bf039fb771b01f44
reppard <reppardwalker@gmail.com>
2014-07-19 22:31:01 -0400

    adding roles to helper and form

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,7 @@
 module MembersHelper
+  def roles
+    ["Member", "Group Leader", "Assistant Leader",
+     "Prayer Leader", "Assistant Prayer Leader",
+     "Ministry Leader", "Assistant Leader"]
+  end
 end
