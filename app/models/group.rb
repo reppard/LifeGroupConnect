221dfcb2f2021595f005c87f66bf5032eba80083
reppard <reppardwalker@gmail.com>
2014-07-20 20:37:41 -0400

    changed members_sorted method name to members_sorted_by_name

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
   has_many :memberships
   has_many :members, :through => :memberships
 
-  def members_sorted
+  def members_sorted_by_name
     self.members.sort_by{ |member| member.name }
   end
 end
