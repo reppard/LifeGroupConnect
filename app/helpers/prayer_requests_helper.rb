df2d4a9a9cb63e9598abd9a1470f2cec52569255
reppard <reppardwalker@gmail.com>
2014-07-22 16:47:07 -0400

    Helper method to get members of group for prayer request form

--- app/helpers/prayer_requests_helper.rb
+++ app/helpers/prayer_requests_helper.rb
@@ -1,2 +1,6 @@
 module PrayerRequestsHelper
+  def members_of_group(group_id)
+    memberships = Membership.where(group_id: group_id)
+    Member.find(memberships.collect{ |m| m.member_id })
+  end
 end
