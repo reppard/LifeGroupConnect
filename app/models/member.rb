015a9dc8e59f7ab993ee4127cc366f1e8b55f8ab
reppard <reppardwalker@gmail.com>
2014-09-06 13:52:02 -0400

    committing unstaged stuff, formatting HD

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,18 @@ class Member < ActiveRecord::Base
   has_many :groups, :through => :memberships
 
   def group_name
-    self.groups.first.name
+    self.groups.first.name unless has_no_groups?
+  end
+
+  def group_id
+    self.groups.first.id unless has_no_groups? 
+  end
+
+  def role
+    self.memberships.first.role unless self.memberships.empty?
+  end
+
+  def has_no_groups?
+    self.groups.empty?
   end
 end
